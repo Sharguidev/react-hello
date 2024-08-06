58dd4ba41d551ac268420a168e7b8eae8520c919
Guillermo Obando <162076693+Sharguidev@users.noreply.github.com>
2024-08-06 03:27:17 +0000

    Add purple color

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [color, setColor] = useState("");
+
+	const [luces, setLuces] = useState([{nombre: 'rojo', valor: 'red'}, {nombre: 'amarillo', valor: 'yellow'}, {nombre: 'verde', valor: 'green'}]);
+	
+ 	return (
+		<div className="container mt-5">
+			<div className="container mt-5 bg-black" style={{width:200, borderRadius: '10%'}}>
+				<div className="d-inline-block d-flex flex-column align-items-center">
+					
+					{
+						luces.map(faro=><button className={"d-inline-block  rounded-circle m-3 " + (color == 'red' ? faro.nombre : '')} style={{height:80, width:80, backgroundColor:faro.valor,}} onClick={()=>{setColor=faro.valor}}/>)
+					
+					/* <button className={"d-inline-block bg-warning rounded-circle m-3 " + (color == 'yellow' ? 'amarillo' : '')} style={{height:80, width:80}} onClick={()=>{
+						setColor("yellow")
+					}}/>
+					<button className={"d-inline-block bg-success rounded-circle m-3 "+ (color =='green' ? 'verde': '')} style={{height:80, width:80}} onClick={()=>{
+						setColor("green")
+					}}/> */}
+				</div>
+			</div>
+			<button className="btn btn-success" onClick={()=>{
+
+				if(luces.length<=3){
+					setLuces([...luces, {nombre: 'morado', valor: 'purple'}])
+				}
+			}}>Agregar</button>
 		</div>
 	);
 };
